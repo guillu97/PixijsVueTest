dcfa9bd019046c07c6205e955f9ce7f0a5715448
guillaume.farge@efrei.net <spore.gui@hotmail.fr>
2020-05-05 21:30:17 +0200

    debug

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,45 +1,45 @@
 <template>
-    <div id="Loader"></div>
+  <div id="Loader"></div>
 </template>
 
 <script>
 import * as PIXI from "pixi.js";
 export default {
-    data: function() {
-        return {};
-    },
-    name: "Loader",
-    components: {},
-    methods: {},
-    created() {
-        console.log("loader created");
-        // load resources
-        const logo1 = PIXI.Texture.from(require("./assets/logo1.png"));
-        const logo2 = PIXI.Texture.from(require("./assets/logo2.png"));
-        const moon = PIXI.Texture.from(require("./assets/moon.jpg"));
-        const sunset = PIXI.Texture.from(require("./assets/sunset.jpg"));
+  data: function() {
+    return {};
+  },
+  name: "Loader",
+  components: {},
+  methods: {},
+  created() {
+    console.log("loader created");
+    // load resources
+    const logo1 = PIXI.Texture.from(require("@/assets/logo1.png"));
+    const logo2 = PIXI.Texture.from(require("@/assets/logo2.png"));
+    const moon = PIXI.Texture.from(require("@/assets/moon.jpg"));
+    const sunset = PIXI.Texture.from(require("@/assets/sunset.jpg"));
 
-        PIXI.Texture.addToCache(logo1, "logo1");
-        PIXI.Texture.addToCache(logo2, "logo2");
-        PIXI.Texture.addToCache(moon, "moon");
-        PIXI.Texture.addToCache(sunset, "sunset");
+    PIXI.Texture.addToCache(logo1, "logo1");
+    PIXI.Texture.addToCache(logo2, "logo2");
+    PIXI.Texture.addToCache(moon, "moon");
+    PIXI.Texture.addToCache(sunset, "sunset");
 
-        console.log("resources added to the cache");
-        this.$emit("resources-added");
-    },
-    mounted() {
-        console.log("loader mounted");
-    },
+    console.log("resources added to the cache");
+    this.$emit("resources-added");
+  },
+  mounted() {
+    console.log("loader mounted");
+  }
 };
 </script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/components/LogoRender.vue
+++ src/components/LogoRender.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="LogoRender">
-        <h1>{{ component_id }}</h1>
-        <div v-bind:id="component_id"></div>
-    </div>
+  <div class="LogoRender">
+    <h1>{{ component_id }}</h1>
+    <div v-bind:id="component_id"></div>
+  </div>
 </template>
 
 <script>
@@ -11,107 +11,107 @@ import * as PIXI from "pixi.js";
 // import { Application } from "pixi.js";
 // import { utils } from "pixi.js";
 export default {
-    name: "LogoRender",
-    props: {
-        component_id: String,
-        logo: String,
-        tint: String,
-    },
-    methods: {
-        display: function() {
-            // if import only utils
-            // let type = "WebGL";
-            // if (!utils.isWebGLSupported()) {
-            //     type = "canvas";
-            // }
-            // utils.sayHello(type);
+  name: "LogoRender",
+  props: {
+    component_id: String,
+    logo: String,
+    tint: String
+  },
+  methods: {
+    display: function() {
+      // if import only utils
+      // let type = "WebGL";
+      // if (!utils.isWebGLSupported()) {
+      //     type = "canvas";
+      // }
+      // utils.sayHello(type);
 
-            // if import all
-            let type = "WebGL";
-            if (!PIXI.utils.isWebGLSupported()) {
-                type = "canvas";
-            }
-            PIXI.utils.sayHello(type);
+      // if import all
+      let type = "WebGL";
+      if (!PIXI.utils.isWebGLSupported()) {
+        type = "canvas";
+      }
+      PIXI.utils.sayHello(type);
 
-            console.log(this.component_id);
-            const app = new PIXI.Application();
-            // if you import only Application
-            // const app = new Application();
+      console.log(this.component_id);
+      const app = new PIXI.Application();
+      // if you import only Application
+      // const app = new Application();
 
-            document.getElementById(this.component_id).appendChild(app.view);
+      document.getElementById(this.component_id).appendChild(app.view);
 
-            console.log("./assets/" + this.logo + ".png");
-            const path = require("./assets/" + this.logo + ".png");
-            console.log(path);
+      console.log("@/assets/" + this.logo + ".png");
+      const path = require("@/assets/" + this.logo + ".png");
+      console.log(path);
 
-            const resourceName = this.logo;
+      const resourceName = this.logo;
 
-            var loader = new PIXI.Loader();
-            if (!PIXI.utils.TextureCache[resourceName]) {
-                // texture is not in cache
-                console.log("texture is not in cache");
-                loader.add(resourceName, this.image_path);
-            } else {
-                // texture is in cache
-                console.log("texture is in cache");
-            }
-            loader.load((loader, resources) => {
-                let texture;
-                if (!PIXI.utils.TextureCache[resourceName]) {
-                    // get the texture from the resources
-                    texture = resources[resourceName].texture;
-                } else {
-                    // get the texture from the cache
-                    texture = PIXI.utils.TextureCache[resourceName];
-                }
+      var loader = new PIXI.Loader();
+      if (!PIXI.utils.TextureCache[resourceName]) {
+        // texture is not in cache
+        console.log("texture is not in cache");
+        loader.add(resourceName, this.image_path);
+      } else {
+        // texture is in cache
+        console.log("texture is in cache");
+      }
+      loader.load((loader, resources) => {
+        let texture;
+        if (!PIXI.utils.TextureCache[resourceName]) {
+          // get the texture from the resources
+          texture = resources[resourceName].texture;
+        } else {
+          // get the texture from the cache
+          texture = PIXI.utils.TextureCache[resourceName];
+        }
 
-                // This creates a texture from a 'logo.png' image.
-                const sprite = new PIXI.Sprite(texture);
+        // This creates a texture from a 'logo.png' image.
+        const sprite = new PIXI.Sprite(texture);
 
-                // add tint if defined
-                if (this.tint) {
-                    sprite.tint = this.tint;
-                }
+        // add tint if defined
+        if (this.tint) {
+          sprite.tint = this.tint;
+        }
 
-                // Setup the position of the sprite
-                sprite.x = app.renderer.width / 2;
-                sprite.y = app.renderer.height / 2;
+        // Setup the position of the sprite
+        sprite.x = app.renderer.width / 2;
+        sprite.y = app.renderer.height / 2;
 
-                // Rotate around the center
-                sprite.anchor.x = 0.5;
-                sprite.anchor.y = 0.5;
+        // Rotate around the center
+        sprite.anchor.x = 0.5;
+        sprite.anchor.y = 0.5;
 
-                // Add the sprite to the scene we are building.
-                app.stage.addChild(sprite);
+        // Add the sprite to the scene we are building.
+        app.stage.addChild(sprite);
 
-                // Listen for frame updates
-                app.ticker.add(() => {
-                    // each frame we spin the sprite around a bit
-                    sprite.rotation += 0.01;
-                });
-            });
-        },
-    },
-    mounted() {
-        this.display();
-    },
+        // Listen for frame updates
+        app.ticker.add(() => {
+          // each frame we spin the sprite around a bit
+          sprite.rotation += 0.01;
+        });
+      });
+    }
+  },
+  mounted() {
+    this.display();
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-    margin: 40px 0 0;
+  margin: 40px 0 0;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-    display: inline-block;
-    margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
